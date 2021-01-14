1486a2edcc8e6708646500d96ad0d6ea9c17e371
edu <eduasinco@gmail.com>
2021-01-14 02:12:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { MainNavComponent } from './main-nav/main-nav.component';
@@ -11,12 +10,47 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ElementsComponent } from './elements/elements.component';
+import { AccountComponent } from './account/account.component';
+import { AddElementComponent } from './add-element/add-element.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+  },
+  {
+    path: 'elements',
+    component: ElementsComponent,
+    children: [
+      {
+        path: 'add',
+        component: AddElementComponent,
+      },
+    ]
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    MainNavComponent
+    MainNavComponent,
+    HomeComponent,
+    ElementsComponent,
+    AccountComponent,
+    AddElementComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +60,11 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    RouterModule.forRoot(routes, {
+      enableTracing: true,
+      useHash: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
